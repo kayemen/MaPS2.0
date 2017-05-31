e221a75eb7447c177aafc5f463a338e3ddb7b7ae
Karan Nair <karan.m.nair@gmail.com>
2017-05-30 20:28:32 -0700

    Modified directory structure
    Changes made to directory structure for making easier relative imports. 
    maps is now the top level package. main.py is simply a script to call the necessary packages

--- main.py
+++ main.py
@@ -1,3 +1,12 @@
 # Main script. Calls all module functions
 
-import settings
+# import settings
+from kivy.config import Config
+
+from maps.gui_windows.maps_ui import MapsApp
+
+window_size = (1000, 750)
+
+Config.set('graphics', 'width', str(window_size[0]))
+Config.set('graphics', 'height', str(window_size[1]))
+MapsApp().run()
--- maps/helpers/gui_modules.py
+++ maps/helpers/gui_modules.py
@@ -36,12 +36,15 @@ def lock_selection(*arg, **kwargs):
     pass
 
 
-def draw_image_overlay(window_name, img, overlay_type=None, overlay_data=None, normalize=True):
+def create_image_overlay(img, overlay_type=None, overlay_data=None, normalize=True):
     # frame = img.copy()
     frame = cv2.cvtColor(img, cv2.COLOR_GRAY2BGR)
     if normalize:
         frame = cv2.normalize(frame, None, 0, 65535, cv2.NORM_MINMAX)
-    if overlay_type == 'rectangle':
+
+    if overlay_type is None:
+        pass
+    elif overlay_type == 'rectangle':
         if overlay_data is None:
             raise GUIException('Rectangle parameters not passed')
         if set(['pt1', 'pt2', 'color']) > set(overlay_data.keys()):
@@ -53,9 +56,11 @@ def draw_image_overlay(window_name, img, overlay_type=None, overlay_data=None, n
         if set(['pts', 'color', 'lineType']) > set(overlay_data.keys()):
             raise GUIException('Required polygon parameters not passed - %s' % (set(overlay_data.keys()) - set()))
     else:
-        raise GUIException('Unknown overlay type')
-    cv2.imshow(window_name, frame)
-    return cv2.waitKey(1) & 0xFF
+        raise GUIException('Unknown overlay type - %s' % overlay_type)
+
+    # print type(frame[0,0,0])
+    frame = (frame / 256).astype('uint8')
+    return frame
 
 
 def load_image_sequence(img_path_list):
@@ -71,18 +76,28 @@ def load_image_sequence(img_path_list):
     return img_seq
 
 
-def max_heartsize_frame_selection(img_seq):
+def max_heartsize_frame(img):
     cv2.namedWindow("MaxHeartframe")
     cv2.setMouseCallback("MaxHeartframe", rectangular_select)
     rect_params = {'color': (0, 65535, 0), 'thickness': 3}
-    key = draw_image_overlay("MaxHeartframe", img_seq[0], overlay_type='rectangle', overlay_data=rect_data)
-    selected_frame = 0
-    cv2.createTrackbar('img_id', 'MaxHeartframe', selected_frame, len(img_seq) - 1, nothing)
-    while key != ord('q'):
+    # frame = create_image_overlay(img_seq[0], overlay_type='rectangle', overlay_data=rect_data)
+    # cv2.imshow("MaxHeartframe", frame)
+    # key = cv2.waitKey(1) & 0xFF
+    # selected_frame = 0
+    # cv2.createTrackbar('img_id', 'MaxHeartframe', selected_frame, len(img_seq) - 1, nothing)
+    # while key != ord('q'):
+    while cv2.getWindowProperty('MaxHeartframe', 0) >= 0:
         # print cv2.getWindowProperty('MaxHeartframe', 0)
-        key = draw_image_overlay("MaxHeartframe", img_seq[cv2.getTrackbarPos('img_id', 'MaxHeartframe')], overlay_type='rectangle',
-                                 overlay_data=rect_data)
+        frame = create_image_overlay(img,
+                                     overlay_type='rectangle',
+                                     overlay_data=rect_data
+                                     )
+        cv2.imshow("MaxHeartframe", frame)
+        key = cv2.waitKey(1) & 0xFF
 
+
+def get_rect_params():
+    return [rect_data['pt1'], rect_data['pt2']]
 
 if __name__ == '__main__':
     path_list = glob.glob('../Data sets/Phase_Bidi/*.tif')
